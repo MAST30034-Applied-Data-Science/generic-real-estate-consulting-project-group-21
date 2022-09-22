e1a8c5f72b3621da5776179ded3544cb6182f91a
̀lachlaǹ <lachymacartney@gmail.com>
2022-09-22 15:40:07 +1000

    paralellise GETs

--- scripts/OpenRouteService.py
+++ scripts/OpenRouteService.py
@@ -2,7 +2,7 @@ import datetime
 import time
 
 import pandas as pd
-import requests
+import grequests
 
 
 def read_csv_file(p='../data/curated/realestate_with_coordinates.csv'):
@@ -15,7 +15,7 @@ def read_csv_file(p='../data/curated/realestate_with_coordinates.csv'):
     return df
 
 
-def post_get(start_longitude, start_latitude, end_longitude, end_latitude):
+def generate_request_url(start_longitude, start_latitude, end_longitude, end_latitude):
     """
     request data
     :param start_longitude: starting point
@@ -24,23 +24,23 @@ def post_get(start_longitude, start_latitude, end_longitude, end_latitude):
     :param end_latitude: end point
     :return: distance， duration
     """
-    headers = {
-        'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
-    }
+    
     # combine URL
     url = 'http://34.143.200.33:8080/ors/v2/directions/driving-car?start={},{}&end={},{}'.format(
         start_longitude, start_latitude, end_longitude, end_latitude)
-    print(url)
-    call = requests.get(url, headers=headers)
-    print(call)
+    
+    return url
+
+   # call = requests.get(url, headers=headers)
+   # print(call)
     # show responses
-    if call.status_code == 200:
-        data = call.json()
-        data = data["features"][0]["properties"]["segments"][0]
-        return data["distance"], data["duration"]
-    else:
-        print(call.status_code, call.reason)
-        return None, None
+   # if call.status_code == 200:
+   #     data = call.json()
+   #     data = data["features"][0]["properties"]["segments"][0]
+   #     return data["distance"], data["duration"]
+   # else:
+   #     print(call.status_code, call.reason)
+   #     return None, None
 
 
 def main():
@@ -73,25 +73,42 @@ def main():
             df.insert(loc=loc + 2, column=column2, value=None)
     # save
     df.to_csv(file_path, index=False)
-
+    urls = []
     for index in df.index:
         for i in range(0, len(columns), 2):
             # check if empty
             if pd.isna(df.loc[index, columns[i]]):
                 # request for empty values
-                distance, duration = post_get(df.loc[index, "longitude"], df.loc[index, "latitude"],
-                                              df.loc[index, position[i + 1]], df.loc[index, position[i]])
-                if not distance is None and not duration is None:
-                    print("success", i//2, index, len(df.index))
+                urls.append(generate_request_url(df.loc[index, "longitude"], df.loc[index, "latitude"],
+                                              df.loc[index, position[i + 1]], df.loc[index, position[i]]))    
+    
+    print(f'making {len(urls)} parallel GET requests...')
+    requests = (grequests.get(u) for u in urls)
+    
+    i = 0
+    percent_complete = 0
+    for response in grequests.imap(requests, size=1000):
+        i += 1
+        if i/len(urls)*100 >= percent_complete:
+            print(f'{percent_complete}% of GET requests complete...')
+            percent_complete += 10
+
+        #print(response)
+        #add duration/distance to url
+    
+    #need to add these columsn to the CSV
+    
+    #            if not distance is None and not duration is None:
+    #                print("success", i//2, index, len(df.index))
                     # unit conversion
                     # distance = distance/1000
                     # duration = duration/60
-                    df.loc[index, columns[i]] = distance
-                    df.loc[index, columns[i+1]] = duration
+    #                df.loc[index, columns[i]] = distance
+    #                df.loc[index, columns[i+1]] = duration
                     # save as requests go through, will start from where it stops if exit in the middle
-                    df.to_csv(file_path, index=False)
-                else:
-                    print('None ', i//2, index, len(df.index))
+    #                df.to_csv(file_path, index=False)
+    #            else:
+    #                print('None ', i//2, index, len(df.index))
 
 
 if __name__ == "__main__":

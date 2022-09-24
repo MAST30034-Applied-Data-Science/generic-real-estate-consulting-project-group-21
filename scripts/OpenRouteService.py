35eb7e54308bb9929e293b5413c9fafd73d20e3f
gy0327 <zuy1@student.unimelb.edu.au>
2022-09-24 15:44:24 +1000

    Update OpenRouteService.py

--- scripts/OpenRouteService.py
+++ scripts/OpenRouteService.py
@@ -2,10 +2,10 @@ import datetime
 import time
 
 import pandas as pd
-import grequests
+import requests
 
 
-def read_csv_file(p='../data/curated/realestate_with_coordinates.csv'):
+def read_csv_file(p='../data/curated/realestate_coor.csv'):
     """
     reads csv
     :param p: directory
@@ -15,7 +15,7 @@ def read_csv_file(p='../data/curated/realestate_with_coordinates.csv'):
     return df
 
 
-def generate_request_url(start_longitude, start_latitude, end_longitude, end_latitude):
+def post_get(start_longitude, start_latitude, end_longitude, end_latitude):
     """
     request data
     :param start_longitude: starting point
@@ -24,33 +24,33 @@ def generate_request_url(start_longitude, start_latitude, end_longitude, end_lat
     :param end_latitude: end point
     :return: distance， duration
     """
-    
+    headers = {
+        'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
+    }
     # combine URL
     url = 'http://34.143.200.33:8080/ors/v2/directions/driving-car?start={},{}&end={},{}'.format(
         start_longitude, start_latitude, end_longitude, end_latitude)
-    
-    return url
+    print(url)
+    try:
+        call = requests.get(url, headers=headers)
+        print(call)
+    except Exception as e:
+        print(e)
+        return None, None
+    else:
+        # show responses
+        if call.status_code == 200:
+            data = call.json()
+            data = data["features"][0]["properties"]["segments"][0]
+            return data["distance"], data["duration"]
+        else:
+            print(call.status_code, call.reason)
+            return None, None
 
-   # call = requests.get(url, headers=headers)
-   # print(call)
-    # show responses
-   # if call.status_code == 200:
-   #     data = call.json()
-   #     data = data["features"][0]["properties"]["segments"][0]
-   #     return data["distance"], data["duration"]
-   # else:
-   #     print(call.status_code, call.reason)
-   #     return None, None
-
-failed_requests = ()
-
-def exception_handler(request, exception):
-    global failed_requests
-    failed_requests += (request,)
 
 def main():
     # file directory
-    file_path = '../data/curated/realestate_with_coordinates.csv'
+    file_path = '../data/curated/realestate_coor.csv'
     # load to dataframe
     df = read_csv_file(file_path)
 
@@ -78,66 +78,33 @@ def main():
             df.insert(loc=loc + 2, column=column2, value=None)
     # save
     df.to_csv(file_path, index=False)
-    urls = []
+
+    write_time = datetime.datetime.now()
     for index in df.index:
         for i in range(0, len(columns), 2):
-            # check if empty
+            # request if empty
+            # change to while to make it pause if request isn't successful
             if pd.isna(df.loc[index, columns[i]]):
-                # request for empty values
-                urls.append(generate_request_url(df.loc[index, "longitude"], df.loc[index, "latitude"],
-                                              df.loc[index, position[i + 1]], df.loc[index, position[i]]))
-    #library doesn't like 60,000 requests at a time, batch them into groups ourselves
-    responses = []
-    global failed_requests
-    percent_complete = 0
-    i = 0
-    BATCH_SIZE = 9323
-    print(f'batching {len(urls)} GET requests into groups of size {BATCH_SIZE}...\n')
-    while (i+1)*BATCH_SIZE < len(urls):
-        requests = (grequests.get(u) for u in urls[i*BATCH_SIZE:(i+1)*BATCH_SIZE])
-        resp_batch = grequests.map(requests,exception_handler=exception_handler)
-        retries = 0
-        while len(failed_requests)>0 and retries < 10:
-            retries += 1
-            failed_requests = ()    
-            resp_batch = grequests.map(requests,exception_handler=exception_handler)
-        if len(failed_requests)>0:
-            print("Requests failed, try again?")
-        responses.append(resp_batch)
-        if (i+1)*BATCH_SIZE/len(urls)*100 >= percent_complete:
-            percent_complete = (i+1)*BATCH_SIZE/len(urls)*100
-            print(f'\r {(i+1)*BATCH_SIZE} ({percent_complete:.1f}%) of GET requests complete...', end='', flush=True)
-        i += 1
-    resp_batch = (grequests.get(u) for u in urls[i*BATCH_SIZE:])
-    retries = 0
-    while len(failed_requests)>0 and retries < 10:
-        retries += 1
-        failed_requests = ()    
-        resp_batch = grequests.map(requests,exception_handler=exception_handler)
-    if len(failed_requests)>0:
-        print("Requests failed, try again?")
-    responses.append(resp_batch)
-    
-    print('\r 100% of GET requests complete... \n', end='', flush=True)
-    
-
-
-        #print(response)
-        #add duration/distance to url
-    
-    #need to add these columsn to the CSV
-    
-    #            if not distance is None and not duration is None:
-    #                print("success", i//2, index, len(df.index))
+                # send request and get data
+                distance, duration = post_get(df.loc[index, "longitude"], df.loc[index, "latitude"],
+                                              df.loc[index, position[i + 1]], df.loc[index, position[i]])
+                if not distance is None and not duration is None:
+                    print("success", i//2, index, len(df.index))
                     # unit conversion
                     # distance = distance/1000
                     # duration = duration/60
-    #                df.loc[index, columns[i]] = distance
-    #                df.loc[index, columns[i+1]] = duration
-                    # save as requests go through, will start from where it stops if exit in the middle
-    #                df.to_csv(file_path, index=False)
-    #            else:
-    #                print('None ', i//2, index, len(df.index))
+                    df.loc[index, columns[i]] = distance
+                    df.loc[index, columns[i+1]] = duration
+                else:
+                    print('None ', i//2, index, len(df.index), "Try requesting again after a second")
+
+        if write_time + datetime.timedelta(seconds=90) < datetime.datetime.now():
+            # write csv every 90 seconds
+            df.to_csv(file_path, index=False)
+            write_time = datetime.datetime.now()
+        else:
+            df.to_csv(file_path, index=False)
+
 
 
 if __name__ == "__main__":

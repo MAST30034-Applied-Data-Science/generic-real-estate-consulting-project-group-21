2bb004b4bf79cfa8a0d3d006ccf87cd9987a3f8d
̀lachlaǹ <lachymacartney@gmail.com>
2022-10-06 13:36:49 +1100

    geoplots

--- scripts/engineer-metrics.py
+++ scripts/engineer-metrics.py
@@ -4,15 +4,14 @@ import pandas as pd
 df = pd.read_csv('data/curated/realestate_with_closest_distance_duration.csv')
 df2 = pd.read_csv('data/curated/realestate_coor_school.csv')[['id', 'min_sec_icsea', 'min_pri_icsea']]
 
-df2['min_pri_icsea'] = pd.to_numeric(df2['min_pri_icsea'], errors='coerce')
-df2['min_sec_icsea'] = pd.to_numeric(df2['min_sec_icsea'], errors='coerce')
-
-df = df.join(df2, lsuffix='id', rsuffix='id')
+df = df.merge(df2, on='id')
+df = df.merge(pd.read_csv('data/curated/Crime_Rate.csv'), left_on='postcode', right_on='Postcode')
 
 df = df[['suburb', 'postcode', 'price', 'propertyType',
         'bedrooms', 'bathrooms', 'parkingSpaces', 'studies', 'furnished',
          'closest_primary_distance', 'closest_secondary_distance',
          'closest_train_distance', 'closest_tram_distance',
+         'cbd_distance', 'cbd_duration', 'zlogCrimerate',
          'closest_bus_distance', 'closest_park_distance', 'min_pri_icsea',
          'min_sec_icsea']]
 

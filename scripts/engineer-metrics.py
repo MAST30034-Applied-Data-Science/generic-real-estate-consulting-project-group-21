85b56d59b72cdb45fd5523a3260aa0ed4b9721d2
̀lachlaǹ <lachymacartney@gmail.com>
2022-10-06 22:12:57 +1100

    question 1

--- scripts/engineer-metrics.py
+++ scripts/engineer-metrics.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from numpy import log
 
 df = pd.read_csv('data/curated/realestate_with_closest_distance_duration.csv')
 df2 = pd.read_csv('data/curated/realestate_coor_school.csv')[['id', 'min_sec_icsea', 'min_pri_icsea']]
@@ -15,15 +15,19 @@ df = df[['suburb', 'postcode', 'price', 'propertyType',
          'closest_bus_distance', 'closest_park_distance', 'min_pri_icsea',
          'min_sec_icsea']]
 
+df = df[(df['closest_park_distance'] > 0) & (df['closest_tram_distance'] > 0) & (df['closest_train_distance'] > 0)]
 
 # Livability
+df['log_closest_park_distance'] = log(df['closest_park_distance'])
+df['log_closest_tram_distance'] = log(df['closest_tram_distance'])
+df['log_closest_train_distance'] = log(df['closest_train_distance'])
 
 df['park_walkability'] = df['closest_park_distance'].apply(lambda dis: r'$<$1.5 km' if dis < 1500 else '1.5-5km' if dis < 5000 else r'$>$5 km')
-df['is_primary_school_walkable'] = df['closest_primary_distance'].apply(lambda dis: 'Yes' if dis < 1500 else 'No')
-df['is_secondary_school_walkable'] = df['closest_secondary_distance'].apply(lambda dis: 'Yes' if dis < 1500 else 'No')
+df['is_primary_school_walkable'] = df['closest_primary_distance'].apply(lambda dis: r'$<$1.5km' if dis < 1500 else r'$>$1.5km')
+df['is_secondary_school_walkable'] = df['closest_secondary_distance'].apply(lambda dis: r'$<$1.5km' if dis < 1500 else r'$>$1.5km')
 
-df['is_tram_stop_walkable'] = df['closest_tram_distance'].apply(lambda dis: 'Yes' if dis < 1500 else 'No')
-df['is_train_station_walkable'] = df['closest_train_distance'].apply(lambda dis: 'Yes' if dis < 1500 else 'No')
+df['is_tram_stop_walkable'] = df['closest_tram_distance'].apply(lambda dis: r'$<$1.5 km' if dis < 1500 else '1.5-5km' if dis < 5000 else r'$>$5 km')
+df['is_train_station_walkable'] = df['closest_train_distance'].apply(lambda dis: r'$<$1.5 km' if dis < 1500 else '1.5-5km' if dis < 5000 else r'$>$5 km')
 
 df['city'] = df['cbd_distance'].apply(lambda dis: dis<3000)
 

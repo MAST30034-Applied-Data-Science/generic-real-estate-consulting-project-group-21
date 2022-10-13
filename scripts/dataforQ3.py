a15ad92aa44b8285dd2dad956ce293a27d690afd
wddin <wddin@student.unimelb.edu.au>
2022-10-13 23:35:41 +1100

    pep8

--- scripts/dataforQ3.py
+++ scripts/dataforQ3.py
@@ -7,8 +7,7 @@ df = df.merge(df2, on='id')
 df = df.merge(pd.read_csv('data/curated/Crime_Rate.csv'), left_on='postcode', right_on='Postcode')
 
 
-df = df[['suburb', 'price', 
-        'bedrooms', 
+df = df[['suburb', 'price', 'bedrooms',
          'closest_primary_distance', 'closest_secondary_distance',
          'closest_train_distance', 'closest_tram_distance', 'Crime Rate',
          'closest_bus_distance', 'closest_park_distance', 'min_pri_icsea',
@@ -23,7 +22,6 @@ df['is_secondary_school_walkable'] = df['closest_secondary_distance'].apply(lamb
 df['is_train_station_walkable'] = df['closest_train_distance'].apply(lambda dis: r'$<$1.5 km' if dis < 1500 else '1.5-5km' if dis < 5000 else r'$>$5 km')
 df['is_tram_walkable'] = df['closest_tram_distance'].apply(lambda dis: r'$<$1.5 km' if dis < 1500 else '1.5-5km' if dis < 5000 else r'$>$5 km')
 df['is_bus_walkable'] = df['closest_bus_distance'].apply(lambda dis: r'$<$1.5 km' if dis < 1500 else '1.5-5km' if dis < 5000 else r'$>$5 km')
-df = df[['suburb', 'price', 'bedrooms', 'park_walkability','is_primary_school_walkable','is_secondary_school_walkable','is_train_station_walkable','Crime Rate','min_pri_icsea','min_sec_icsea','is_tram_walkable','is_bus_walkable']]
-         
+df = df[['suburb', 'price', 'bedrooms', 'park_walkability', 'is_primary_school_walkable', 'is_secondary_school_walkable', 'is_train_station_walkable', 'Crime Rate', 'min_pri_icsea', 'min_sec_icsea', 'is_tram_walkable', 'is_bus_walkable']]
 
 df.to_csv('data/curated/Q3engineered-data.csv', index=False)
